dde876186b35684f219dd7fb559dd5da3186da7e
fran <franciscojavierarevalo@hotmail.com>
2025-06-08 12:05:38 +0200

    Agregar componente de factura y actualizar configuración de la aplicación

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { Invoice } from './app/components/invoice/invoice';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(Invoice, appConfig)
   .catch((err) => console.error(err));
